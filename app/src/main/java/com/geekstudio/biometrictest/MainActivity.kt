1cbcfda9da816f50ae55e7a2d41357438a02d8e2
BWooRam <wrbae@hyundaiht.co.kr>
2023-11-14 16:49:39 +0900

    [init] Biometric 주석 추가

--- app/src/main/java/com/geekstudio/biometrictest/MainActivity.kt
+++ app/src/main/java/com/geekstudio/biometrictest/MainActivity.kt
@@ -27,7 +27,10 @@ class MainActivity : AppCompatActivity() {
         val manager = BiometricManager.from(this@MainActivity)
         when (manager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)) {
             BiometricManager.BIOMETRIC_SUCCESS -> {
+                //오류가 감지되지 않았습니다.
                 Toast.makeText(this@MainActivity, "status = BIOMETRIC_SUCCESS", Toast.LENGTH_SHORT).show()
+
+                //개인키 생성
                 generateSecretKey(
                     KeyGenParameterSpec.Builder(
                         KEY_NAME,
@@ -46,18 +49,37 @@ class MainActivity : AppCompatActivity() {
                 )
                 showBiometric()
             }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                //생체 인식 하드웨어가 없습니다.
+                Toast.makeText(this@MainActivity, "status = BIOMETRIC_ERROR_NO_HARDWARE", Toast.LENGTH_SHORT).show()
+            }
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                //하드웨어를 사용할 수 없습니다.
+                Toast.makeText(this@MainActivity, "status = BIOMETRIC_ERROR_HW_UNAVAILABLE", Toast.LENGTH_SHORT).show()
+            }
             BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                //사용자에게 등록된 생체 인식이 없습니다.
                 Toast.makeText(this@MainActivity, "status = BIOMETRIC_ERROR_NONE_ENROLLED", Toast.LENGTH_SHORT).show()
+            }
+            BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED -> {
+                //보안 취약점이 발견되었으며 보안 업데이트로 이 문제가 해결될 때까지 센서를 사용할 수 없습니다.
+                Toast.makeText(this@MainActivity, "status = BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED", Toast.LENGTH_SHORT).show()
             }
 
             else -> Toast.makeText(this@MainActivity, "status = else type", Toast.LENGTH_SHORT).show()
         }
     }
 
+    /**
+     *  생체 인증 팝업 초기화
+     */
     private fun initBiometricPopUp() {
         val executor = ContextCompat.getMainExecutor(this)
         biometricPrompt = BiometricPrompt(this, executor,
             object : BiometricPrompt.AuthenticationCallback() {
+                /**
+                 * 복구할 수 없는 오류가 발생하여 인증이 중지된 경우 호출됩니다.
+                 */
                 override fun onAuthenticationError(
                     errorCode: Int,
                     errString: CharSequence
@@ -70,11 +92,15 @@ class MainActivity : AppCompatActivity() {
                         .show()
                 }
 
+                /**
+                 * 생체인식(예: 지문, 얼굴 등)이 제시되었으나 사용자의 것으로 인식되지 않는 경우 호출됩니다.
+                 */
                 override fun onAuthenticationSucceeded(
                     result: BiometricPrompt.AuthenticationResult
                 ) {
                     super.onAuthenticationSucceeded(result)
 
+                    //기기 사용자 인증 정보와 생체 인식 사용자 인증 정보 중 어떤 정보가 사용되었는지 확인할 수 있습니다.
                     when(result.authenticationType){
                         BiometricPrompt.AUTHENTICATION_RESULT_TYPE_UNKNOWN -> {
                             //사용자가 알 수 없는 방법을 통해 인증한 경우 보고되는 인증 유형입니다 .
@@ -98,6 +124,9 @@ class MainActivity : AppCompatActivity() {
                         .show()
                 }
 
+                /**
+                 * 생체 인식(예: 지문, 얼굴 등)이 인식되면 호출되어 사용자가 성공적으로 인증되었음을 나타냅니다.
+                 */
                 override fun onAuthenticationFailed() {
                     super.onAuthenticationFailed()
                     Toast.makeText(
@@ -109,6 +138,9 @@ class MainActivity : AppCompatActivity() {
             })
     }
 
+    /**
+     * 생체 인증 3단계 요청
+     */
     private fun showBiometric(){
         val build = BiometricPrompt.PromptInfo.Builder()
             .setTitle("Biometric login for my app")
@@ -133,6 +165,9 @@ class MainActivity : AppCompatActivity() {
         keyGenerator.generateKey()
     }
 
+    /**
+     *
+     */
     private fun getSecretKey(): SecretKey? {
         val keyStore = KeyStore.getInstance("AndroidKeyStore")
         Log.d(TAG, "keyStore = $keyStore")
@@ -142,6 +177,9 @@ class MainActivity : AppCompatActivity() {
         return keyStore.getKey(KEY_NAME, null) as? SecretKey
     }
 
+    /**
+     *
+     */
     private fun getCipher(): Cipher {
         return Cipher.getInstance(
             KeyProperties.KEY_ALGORITHM_AES + "/"
